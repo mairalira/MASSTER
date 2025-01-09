1593411b2e791d0d7f1d5b9ac62e64438443cd51
Luisa Nogueira Costa Cavalcante <luisa.cavalcante@soupilar.com.br>
2025-01-09 09:49:46 -0300

    Updated pct file

--- src/models/pct.py
+++ src/models/pct.py
@@ -3,6 +3,95 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import explained_variance_score, r2_score, mean_absolute_error
 import time
 import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
+import config
+import csv
+import os
+
+
+# Função para calcular as métricas
+def calculate_metrics(y_true, y_pred):
+    """Calcula R2, MSE, MAE, CA e ARRMSE."""
+    r2 = r2_score(y_true, y_pred)
+    mse = np.mean((y_true - y_pred) ** 2)
+    mae = mean_absolute_error(y_true, y_pred)
+    ca = custom_accuracy(y_true, y_pred)
+    arrmse = np.sqrt(mse / np.mean(y_true ** 2))  # Relative RMSE
+    return r2, mse, mae, ca, arrmse
+
+# Função para calcular as métricas médias por método
+def calculate_mean_performances(method_results, method_name):
+    """Calcula as médias das métricas para um método."""
+    r2_list, mse_list, mae_list, ca_list, arrmse_list = [], [], [], [], []
+
+    for y_true, y_pred in method_results:
+        r2, mse, mae, ca, arrmse = calculate_metrics(y_true, y_pred)
+        r2_list.append(r2)
+        mse_list.append(mse)
+        mae_list.append(mae)
+        ca_list.append(ca)
+        arrmse_list.append(arrmse)
+
+    mean_r2 = np.mean(r2_list)
+    mean_mse = np.mean(mse_list)
+    mean_mae = np.mean(mae_list)
+    mean_ca = np.mean(ca_list)
+    mean_arrmse = np.mean(arrmse_list)
+
+    print(f"{method_name}: Mean R2={mean_r2:.3f}, Mean MSE={mean_mse:.3f}, "
+          f"Mean MAE={mean_mae:.3f}, Mean CA={mean_ca:.3f}, Mean ARRMSE={mean_arrmse:.3f}")
+    return mean_r2, mean_mse, mean_mae, mean_ca, mean_arrmse
+
+def data_read(dataset, data_dir,dataset_name):
+        # split the csv file in the input and target values
+        folder_dir = data_dir / 'processed' / f'{dataset_name}'
+        data_path = folder_dir / f'{dataset}'
+        df = pd.read_csv(data_path)
+
+        # obtain the column names
+        col_names = list(df.columns)
+        target_length = 0
+
+        for name in col_names: 
+            if 'target' in name:
+                target_length += 1
+
+        target_names = col_names[-target_length:]
+
+        inputs = list()
+        targets = list()
+        for k in range(len(df)):
+            input_val = list()
+            target_val = list()
+            for col in col_names:
+                if col in target_names:
+                    target_val.append(df.loc[k, col])
+                else:
+                    input_val.append(df.loc[k, col])
+            inputs.append(input_val)
+            targets.append(target_val)
+
+        n_instances = len(targets)
+        return inputs, targets, n_instances, target_length
+
+# Helper function to read data
+def read_data(iteration,data_dir,dataset_name): 
+    X_train, y_train, _, _ = data_read(f'train_{iteration}',data_dir,dataset_name)
+    X_pool, y_pool, n_pool, target_length = data_read(f'pool_{iteration}',data_dir,dataset_name)
+    X_rest, y_rest, _, _ = data_read(f'train+pool_{iteration}',data_dir,dataset_name)
+    X_test, y_test, _, _ = data_read(f'test_{iteration}',data_dir,dataset_name)
+    return X_train, y_train, X_pool, y_pool, X_rest, y_rest, X_test, y_test, target_length
+
+def get_datasets(k,data_dir,dataset_name):
+    X_train_not_missing, Y_train_not_missing, X_unlabeled,Y_train_missing,X_rest, y_rest, X_test_labeled, y_test_labeled, target_length = read_data(k+1,data_dir,dataset_name)
+    X_train_not_missing = np.array(X_train_not_missing)
+    Y_train_not_missing = np.array(Y_train_not_missing)
+    X_unlabeled = np.array(X_unlabeled)
+    X_test_labeled = np.array(X_test_labeled)
+    y_test_labeled = np.array(y_test_labeled)
+
+    return X_train_not_missing,Y_train_not_missing,X_unlabeled,X_test_labeled,y_test_labeled,Y_train_missing
+
 
 class PCTNode:
     def __init__(self, depth=0):
@@ -94,87 +183,115 @@ def custom_accuracy(y_true, y_pred, threshold=0.1):
     """Custom accuracy: percentage of predictions within a certain threshold."""
     return np.mean(np.abs(y_true - y_pred) <= threshold)
 
-if __name__ == "__main__":
-    df_train_not_missing = pd.read_csv(r'C:\Users\danin\Documents\lu\mestrado\artigo_ricardo\Active-and-Self-Learning-for-Multi-target-Regression\src\processed\atp7d\train_1')
-    df_train_missing = pd.read_csv(r'C:\Users\danin\Documents\lu\mestrado\artigo_ricardo\Active-and-Self-Learning-for-Multi-target-Regression\src\processed\atp7d\pool_1')
-    df_test = pd.read_csv(r'C:\Users\danin\Documents\lu\mestrado\artigo_ricardo\Active-and-Self-Learning-for-Multi-target-Regression\src\processed\atp7d\test_1')
+data_dir = config.DATA_DIR
+#dataset_name = config.DATASET_NAME
+dataset_names = ['atp7d', 'friedman', 'mp5spec', 'musicOrigin2', 'rf2', 'oes97', 'enb', 'osales', 'wq']
+for dataset_name in dataset_names:
+    print(dataset_name)
+    number_of_pools = 10
+    for i in range (number_of_pools):
+        print(f"Treinando modelo no pool {i}...")  
+        X_train_not_missing, Y_train_not_missing, X_unlabeled, X_test_labeled, y_test_labeled, Y_train_missing = get_datasets(i,data_dir,dataset_name)
+        # ------------------ Semissupervisionado ------------------
+        start_time = time.time()
+        w = 0.5  # Adjust this parameter to control influence (0.0 - only labeled data, 1.0 - equal influence)
+        pct_original = PredictiveClusteringTree(max_depth=5, min_samples_split=10)
+        pct_original.fit(X_train_not_missing, Y_train_not_missing)
+        # Preencher valores faltantes com previsões do modelo original
+        Y_train_filled = Y_train_missing.copy()
 
-    X_test=df_test.drop(columns = ['target_LBL+ALLminpA+bt7d_000','target_LBL+ALLminp0+bt7d_000','target_LBL+aDLminpA+bt7d_000','target_LBL+aCOminpA+bt7d_000','target_LBL+aFLminpA+bt7d_000','target_LBL+aUAminpA+bt7d_000'])
-    Y_test = df_test[['target_LBL+ALLminpA+bt7d_000','target_LBL+ALLminp0+bt7d_000','target_LBL+aDLminpA+bt7d_000','target_LBL+aCOminpA+bt7d_000','target_LBL+aFLminpA+bt7d_000','target_LBL+aUAminpA+bt7d_000']]
+        for j in range(len(Y_train_filled)):
+            # Verifica se a linha contém algum NaN
+            if any(pd.isna(Y_train_filled[j])):
+                # Obtém os valores da linha correspondente de X_train_missing
+                x_missing = X_unlabeled[j, :].reshape(1, -1)
 
-    X_train_missing = df_train_missing.drop(columns = ['target_LBL+ALLminpA+bt7d_000','target_LBL+ALLminp0+bt7d_000','target_LBL+aDLminpA+bt7d_000','target_LBL+aCOminpA+bt7d_000','target_LBL+aFLminpA+bt7d_000','target_LBL+aUAminpA+bt7d_000'])
-    Y_train_missing = df_train_missing[['target_LBL+ALLminpA+bt7d_000','target_LBL+ALLminp0+bt7d_000','target_LBL+aDLminpA+bt7d_000','target_LBL+aCOminpA+bt7d_000','target_LBL+aFLminpA+bt7d_000','target_LBL+aUAminpA+bt7d_000']]
-    Y_train_missing.loc[:, :] = np.nan  # Substitui todos os valores do DataFrame por np.nan
-    X_train_not_missing = df_train_not_missing.drop(columns = ['target_LBL+ALLminpA+bt7d_000','target_LBL+ALLminp0+bt7d_000','target_LBL+aDLminpA+bt7d_000','target_LBL+aCOminpA+bt7d_000','target_LBL+aFLminpA+bt7d_000','target_LBL+aUAminpA+bt7d_000'])
-    Y_train_not_missing = df_train_not_missing[['target_LBL+ALLminpA+bt7d_000','target_LBL+ALLminp0+bt7d_000','target_LBL+aDLminpA+bt7d_000','target_LBL+aCOminpA+bt7d_000','target_LBL+aFLminpA+bt7d_000','target_LBL+aUAminpA+bt7d_000']]
+                # Prediz os valores faltantes usando o modelo original
+                y_pred = pct_original.predict(x_missing)
+                
+                # Preenche os valores previstos em Y_train_filled
+                Y_train_filled.iloc[j, :] = y_pred
+
+        # Combine os datasets
+        if isinstance(X_unlabeled, np.ndarray):
+            X_unlabeled = pd.DataFrame(X_unlabeled, columns=[f'feature_{j}' for j in range(X_unlabeled.shape[1])])
+        if isinstance(X_train_not_missing, np.ndarray):
+            X_train_not_missing = pd.DataFrame(X_train_not_missing, columns=[f'feature_{j}' for j in range(X_train_not_missing.shape[1])])
+        
+        X_train_combined = pd.concat([X_unlabeled, X_train_not_missing], axis=0)
+        X_train_combined.reset_index(drop=True, inplace=True)
+
+        if isinstance(Y_train_filled, np.ndarray):
+            Y_train_filled = pd.DataFrame(Y_train_filled, columns=[f'feature_{j}' for j in range(Y_train_filled.shape[1])])
+        if isinstance(Y_train_not_missing, np.ndarray):
+            Y_train_not_missing = pd.DataFrame(Y_train_not_missing, columns=[f'feature_{j}' for j in range(Y_train_not_missing.shape[1])])
+        if isinstance(Y_train_filled, list):
+            Y_train_filled = pd.DataFrame(Y_train_filled, columns=[f'feature_{j}' for j in range(len(Y_train_filled[0]))])
+        if isinstance(Y_train_missing, list):
+            Y_train_missing = pd.DataFrame(Y_train_missing, columns=[f'feature_{j}' for j in range(len(Y_train_missing[0]))])
+
+        Y_train_combined = pd.concat([Y_train_filled, Y_train_not_missing], axis=0)
+        Y_train_combined.reset_index(drop=True, inplace=True)
+
+        # Criar uma máscara para instâncias rotuladas (não faltantes)
+        labeled_mask = ~Y_train_missing.isna().any(axis=1)
+
+        # Criar o vetor de pesos
+        weights_filled = np.full(Y_train_filled.shape[0], w)  # Pesos para instâncias preenchidas
+        weights_not_missing = np.ones(np.sum(labeled_mask))  # Pesos para instâncias rotuladas originais
+        weights = np.hstack((weights_not_missing, weights_filled))
+
+        # Treinar novo modelo semissupervisionado
+        pct_semi_weighted = PredictiveClusteringTree(max_depth=5, min_samples_split=10)
+        pct_semi_weighted.fit(X_train_combined, Y_train_combined)
+
+        # Fazer previsões no conjunto de teste
+        Y_pred_semi_weighted = pct_semi_weighted.predict(X_test_labeled)
+
+        # Medir o tempo final
+        end_time = time.time()
     
-    
-    # ------------------ Modelo Original ------------------
-    start_time = time.time()
-    #not_missing_mask = ~np.isnan(Y_train_missing).any(axis=1)
+        print("Desempenho Semissupervisionado:")
+    # Coleta dos resultados para cálculo consolidado
+        proposed_method_pct = list(zip(y_test_labeled, Y_pred_semi_weighted))
 
-
-    pct_original = PredictiveClusteringTree(max_depth=5, min_samples_split=10)
-    pct_original.fit(X_train_not_missing.to_numpy(), Y_train_not_missing.to_numpy())
-    Y_pred_original = pct_original.predict(X_test.to_numpy())
-    end_time = time.time()
-
-    print("Desempenho Original:")
-    for i in range(Y_test.shape[1]):
-        r2 = r2_score(Y_test.to_numpy()[:, i], Y_pred_original[:, i])
-        mae = mean_absolute_error(Y_test.to_numpy()[:, i], Y_pred_original[:, i])
-        ca = custom_accuracy(Y_test.to_numpy()[:, i], Y_pred_original[:, i])
-        print(f"Target {i+1}: R2={r2:.3f}, MAE={mae:.3f}, CA={ca:.3f}")
-    print(f"Tempo de execução: {end_time - start_time:.2f} segundos\n")
-
-    # ------------------ Semissupervisionado ------------------
-    start_time = time.time()
-    w = 0.5  # Adjust this parameter to control influence (0.0 - only labeled data, 1.0 - equal influence)
-
-    # Preencher valores faltantes com previsões do modelo original
-    Y_train_filled = Y_train_missing.copy()
-
-    for i in range(Y_train_filled.shape[0]):
-        # Verifica se a linha contém algum NaN
-        if Y_train_filled.iloc[i, :].isna().any():
-            # Obtém os valores da linha correspondente de X_train_missing
-            x_missing = X_train_missing.iloc[i, :].values.reshape(1, -1)
+        # Métricas para o método proposto
+        pct_mean_R2, pct_mean_MSE, pct_mean_MAE, pct_mean_CA, pct_mean_ARRMSE = \
+            calculate_mean_performances(proposed_method_pct, 'Proposed Method')
             
-            # Prediz os valores faltantes usando o modelo original
-            y_pred = pct_original.predict(x_missing)
-            
-            # Preenche os valores previstos em Y_train_filled
-            Y_train_filled.iloc[i, :] = y_pred
 
-    # Combine os datasets
-    X_train_combined = pd.concat([X_train_missing, X_train_not_missing], axis=0)
-    X_train_combined.reset_index(drop=True, inplace=True)
+        # Supondo que as métricas foram calculadas como abaixo:
+        metrics = {
+            "method": [
+                "pct"
+            ],
+            "mean_R2": [
+                pct_mean_R2,
+            ],
+            "mean_MSE": [
+                pct_mean_MSE,
+            ],
+            "mean_MAE": [
+                pct_mean_MAE,
+            ],
+            "mean_CA": [
+                pct_mean_CA,
+            ],
+            "mean_ARRMSE": [
+                pct_mean_ARRMSE,
+            ],
+        }
 
-    Y_train_combined = pd.concat([Y_train_filled, Y_train_not_missing], axis=0)
-    Y_train_combined.reset_index(drop=True, inplace=True)
+        # Caminho para salvar o arquivo CSV
 
-    # Criar uma máscara para instâncias rotuladas (não faltantes)
-    labeled_mask = ~Y_train_missing.isna().any(axis=1)
+        output_dir = rf"C:\Users\danin\Documents\lu\mestrado\artigo_ricardo\Active-and-Self-Learning-for-Multi-target-Regression\reports\semi_supervised\pct\{dataset_name}"
+        output_file = rf"{output_dir}\{i}.csv"
 
-    # Criar o vetor de pesos
-    weights_filled = np.full(Y_train_filled.shape[0], w)  # Pesos para instâncias preenchidas
-    weights_not_missing = np.ones(np.sum(labeled_mask))  # Pesos para instâncias rotuladas originais
-    weights = np.hstack((weights_not_missing, weights_filled))
+        # Cria o diretório se não existir
+        os.makedirs(output_dir, exist_ok=True)
 
-    # Treinar novo modelo semissupervisionado
-    pct_semi_weighted = PredictiveClusteringTree(max_depth=5, min_samples_split=10)
-    pct_semi_weighted.fit(X_train_combined.to_numpy(), Y_train_combined.to_numpy())
+        # Convertendo o dicionário para um DataFrame e salvando no formato CSV
+        df_metrics = pd.DataFrame(metrics)
+        df_metrics.to_csv(output_file, index=False)
 
-    # Fazer previsões no conjunto de teste
-    Y_pred_semi_weighted = pct_semi_weighted.predict(X_test.to_numpy())
-
-    # Medir o tempo final
-    end_time = time.time()
-
-    print("Desempenho Semissupervisionado:")
-    for i in range(Y_test.shape[1]):
-        r2 = r2_score(Y_test.to_numpy()[:, i], Y_pred_original[:, i])
-        mae = mean_absolute_error(Y_test.to_numpy()[:, i], Y_pred_original[:, i])
-        ca = custom_accuracy(Y_test.to_numpy()[:, i], Y_pred_original[:, i])
-        print(f"Target {i+1}: R2={r2:.3f}, MAE={mae:.3f}, CA={ca:.3f}")
-    print(f"Tempo de execução: {end_time - start_time:.2f} segundos\n")
+        print(f"Métricas salvas em '{output_file}'.")
